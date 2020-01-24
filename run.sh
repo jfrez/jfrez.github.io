7811bfd23e9deffba743cd4333f28f429ed75572
Jfrez <jonathan.frez@gmail.com>
2020-01-24 14:10:06 -0300

    .

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 export DYLD_LIBRARY_PATH=/usr/local/Cellar/openssl\@1.1/1.1.1d/lib/
-source activate DSRegressor; python3 /Users/jfrez/Predictor/Predictor/DSRegressor/stock.py ; source deactivate
+source activate DSRegressor; python3 /Users/jfrez/Predictor/Predictor/DSRegressor/stock.py
 python3 ../Predictor/getPred.py > pred.json
 git add .
 git commit -m "."
