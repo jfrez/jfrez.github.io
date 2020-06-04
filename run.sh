9f43d6bd8935a1abd4a4619048207c2726218074
jfrez <jonathan.frez@gmail.com>
2020-06-04 17:00:54 -0400

    .

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-conda activate DSRegressor; python3 ../Predictor/DSRegressor/stock.py
+conda activate DSRegressor
+python3 ../Predictor/DSRegressor/stock.py
 python3 ../Predictor/getPred.py > pred.json
 git add .
 git commit -m "."
